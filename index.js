2da939421e01ed87f4dcfc0323fe09356e78f771
RoryLetteney <rory.letteney@gmail.com>
2019-06-11 16:23:28 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+#!/usr/bin/env node
+
+"use strict";
+
+const chalk = require('chalk');
+const inquirer = require('inquirer');
+const figlet = require('figlet');
+const shell = require('shelljs');
+
+const run = () => {
+  // Collect project name
+  // Create new directory
+  // CD into new directory
+};
+
+run();
