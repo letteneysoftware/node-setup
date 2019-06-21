b2961d49fc822fca75bec628c17334431a3cfa5f
RoryLetteney <rory.letteney@gmail.com>
2019-06-20 20:39:50 -0500

    Added a little extra spacing to createHead for readability.

--- file-system/ejs/createHead.js
+++ file-system/ejs/createHead.js
@@ -8,10 +8,10 @@ const createHead = () => {
 
   let lines = [
     '<head>\n',
-    ' <meta charset="UTF-8">\n',
-    ' <meta name="viewport" content="width=device-width, initial-scale=1.0">\n',
-    ' <meta http-equiv="X-UA-Compatible" content="ie=edge">\n',
-    ' <title>Document</title>\n',
+    '  <meta charset="UTF-8">\n',
+    '  <meta name="viewport" content="width=device-width, initial-scale=1.0">\n',
+    '  <meta http-equiv="X-UA-Compatible" content="ie=edge">\n',
+    '  <title>Document</title>\n',
     '</head>'
   ]
 
